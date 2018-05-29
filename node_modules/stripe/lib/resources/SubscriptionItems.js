1f57b3536d0aa81b8add4bddb77b45bea781b688
simalexan <alexander.simovic@gmail.com>
2018-05-29 18:47:18 +0200

    removing wrongly added modules

--- node_modules/stripe/lib/resources/SubscriptionItems.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var StripeResource = require('../StripeResource');
-
-module.exports = StripeResource.extend({
-  path: 'subscription_items',
-  includeBasic: ['create', 'list', 'retrieve', 'update', 'del',],
-});
-
--- node_modules/stripe/lib/resources/Topups.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-var StripeResource = require('../StripeResource');
-
-module.exports = StripeResource.extend({
-  path: 'topups',
-  includeBasic: ['create', 'list', 'retrieve', 'update', 'setMetadata', 'getMetadata'],
-});
