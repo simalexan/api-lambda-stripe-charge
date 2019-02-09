c9f704abd472a6d5c9b8fd330da71aac134855ca
Aleksandar Simovic <alexander.simovic@gmail.com>
2019-02-09 21:40:48 +0100

    adding SSM params usage

--- index.js
+++ index.js
@@ -1,30 +1,37 @@
 const AWS = require('aws-sdk'),
-    qs = require('querystring'),
-    processResponse = require('./process-response'),
-    stripe = require('stripe')(process.env.STRIPE_SECRET_KEY),
-    IS_CORS = true;
+  ssm = new AWS.SSM(),
+  qs = require('querystring'),
+  processResponse = require('./process-response'),
+  STRIPE_SECRET_KEY_NAME = `${process.env.SSM_PARAMETER_PREFIX}/stripe_secret_key`,
+  IS_CORS = true;
 
 exports.handler = (event) => {
-    if (event.httpMethod === 'OPTIONS') {
-		return Promise.resolve(processResponse(IS_CORS));
-    }
-    if (!event.body) {
-        return Promise.resolve(processResponse(IS_CORS, 'invalid', 400));
-    }
-    const newCharge = qs.parse(event.body);
-    if (!newCharge.amount || !newCharge.currency) {
-        return Promise.resolve(processResponse(IS_CORS, 'invalid arguments, please provide amount and currency fields as mentioned in the app README', 400));
-    }
+  if (event.httpMethod === 'OPTIONS') {
+    return Promise.resolve(processResponse(IS_CORS));
+  }
+  if (!event.body) {
+    return Promise.resolve(processResponse(IS_CORS, 'invalid', 400));
+  }
+  const newCharge = qs.parse(event.body);
+  if (!newCharge.amount || !newCharge.currency) {
+    return Promise.resolve(processResponse(IS_CORS, 'invalid arguments, please provide amount and currency fields as mentioned in the app README', 400));
+  }
 
-	return stripe.charges.create({
-		source: newCharge.stripeToken,
-		amount: newCharge.amount,
-		currency: newCharge.currency,
-		description: 'Charge Description'
+  return ssm.getParameter({ Name: STRIPE_SECRET_KEY_NAME, WithDecryption: true })
+    .then(paramResult => {
+      const STRIPE_SECRET_KEY_VALUE = paramResult[STRIPE_SECRET_KEY_NAME];
+      const stripe = require('stripe')(STRIPE_SECRET_KEY_VALUE);
+
+      return stripe.charges.create({
+        source: newCharge.stripeToken,
+        amount: newCharge.amount,
+        currency: newCharge.currency,
+        description: 'Charge Description'
+      });
     })
-    .then(charge => processResponse(IS_CORS, {createdCharge: charge}))
+    .then(charge => processResponse(IS_CORS, { createdCharge: charge }))
     .catch((err) => {
-        console.log(err);
-        return processResponse(IS_CORS, {err}, 500);
-	});
+      console.log(err);
+      return processResponse(IS_CORS, { err }, 500);
+    });
 };
